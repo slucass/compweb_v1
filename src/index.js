c4c0651fa0f55e344b4dc9881f63a48a1b7bddf0
comperari <36984289+slucass@users.noreply.github.com>
2021-06-08 17:48:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 function Greetings(){
-  return <h4>Comperari - Sales Operations Management and consulting</h4>
+  return <h4>Comperari - Sales Operations Management and consulting and maybe other activities I do not know yet</h4>
 };
 
 ReactDom.render(<Greetings />, document.getElementById('root'));
