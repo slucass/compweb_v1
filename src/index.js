f05b55aabb18c97f93bada7e12b496ddb2ab4390
slucass <info@comperari.com>
2021-06-09 09:54:15 +0200

    sth

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 function Greetings(){
-  return <h4>Comperari - Sales Operations Management and consulting which is the best</h4>
+  return <h4>Comperari - Sales Operations Management</h4>
 };
 
 ReactDom.render(<Greetings />, document.getElementById('root'));
