4a42b7ba60dc84e143abd2bbf057412bd74b8768
slucass <info@comperari.com>
2021-07-02 14:50:14 +0200

    sth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home/Home'
 
 
 function App() {
     return (
         <div >
             <Navbar />
+            <Home />
         </div>
     )
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./Home.css"
+
+const Home = () => {
+    return (
+          
+        <h1 className="container">The company name reflects the meaning of 
+        Computer Operations. We provide sales, project and customer 
+        relationship management in the information technology and 
+        electronics business. Expertise in main market segments: automotive, 
+        industry 4.0 and financial technology. Together with our business 
+        partners we deliver customized, scalable and flexible solution as 
+        well as take care about the complete communication through 
+        the customers, lead software houses, freelance engineers and any 
+        parties involved in the project development process until it 
+        is done and makes the customer satisfied. The core advantage 
+        is flexibility
+         in building virtual teams of worldwide professionals.</h1>
+
+
+
+
+
+
+
+    );
+};
+
+
+export default Home;
